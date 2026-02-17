18cf48dfa4d4763b58d3622bf5d51b77e17f6d1c
p1-games <p1blacketer@gmail.com>
2026-02-17 23:45:37 +0000

    did practice

--- lesson1/classwork/practice.py
+++ lesson1/classwork/practice.py
@@ -5,20 +5,26 @@ print(d)
 
 # Problem 2
 # Create a variable for your height in meters and print it.
-h = 30
+height = 30
+print(height)
 
 
 # Problem 3
 # Create a variable for your favorite animal and print it in a full sentence.
-
-
+My favorite animal tis = "monkey"
+print("My favorite animal tis",My favorite animal tis)
 
 # Problem 4
 # Create two variables: one for your first name and one for your last name.
 # Print them on 2 separate lines.
-
-
-
+first = "Patrick"
+last = "Blacketer"
+print(first)
+print(last)
 # Problem 5
 # Create 3 variables: your first name, your age, and your favorite color.
 # Print them on the same line in a full sentence.
+first2 = "Patrick tis"
+age = "11"
+color = " and likes green"
+print(first2,age,color)
