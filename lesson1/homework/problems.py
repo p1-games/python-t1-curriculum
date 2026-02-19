22a6cd227d4ab36d8756fa8b5cc809401153e2f0
p1-games <p1blacketer@gmail.com>
2026-02-19 01:15:48 +0000

    homework yes

--- lesson1/homework/problems.py
+++ lesson1/homework/problems.py
@@ -1,24 +1,29 @@
 # Problem 1
 # Create a variable for your lucky number and print it.
-
-
+luck = "7"
+print(luck)
 
 # Problem 2
 # Create a variable for the number of hours you sleep and print it.
-
-
+sleep = 8.5
+print(sleep)
 
 # Problem 3
 # Create a variable for your favorite fruit and print it in a full sentence.
-
-
+fruit = "apple"
+print("Patrick's favorit food is a",fruit)
 
 # Problem 4
 # Create two variables: one for your city and one for your country.
 # Print them on 2 separate lines.
-
-
-
+city = "Seattle"
+country = "USA"
+print(city)
+print(country)
 # Problem 5
 # Create 3 variables: your pet's name, its age, and its type.
 # Print them on the same line in a full sentence.
+pet_name = "nonexisent"
+age = "-2"
+type = "no"
+print("my pet is,",pet_name,"and is,",age,"years old. oh and they are a",type)
