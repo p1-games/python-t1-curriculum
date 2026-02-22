dea52eb72451fa562c759bfb7840cab6ea16ffd3
p1-games <p1blacketer@gmail.com>
2026-02-22 01:57:32 +0000

    stuff

---
+++ lesson2/classwork/input.py
@@ -0,0 +1,9 @@
+user_input = input("give me an intiger:")
+print(user_input)
+
+#print(user_input + 1) no
+
+number = int(user_input)
+
+user_number = int(input("give me an intiger:"))
+print(user_number + 1)
---
+++ lesson2/classwork/math_operators.py
@@ -0,0 +1,12 @@
+print(4 + 5) # addition
+
+print(10 - 3) # subbtraction
+
+print(6 * 7) # multiplcation
+
+print(5 / 2) # divition
+ 
+a = 5
+b = 4
+print(a + b)
+print(a * b)
