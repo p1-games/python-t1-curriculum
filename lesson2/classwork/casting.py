2fc309e4596bfe416da2687f972c9b33eb81860d
p1-games <p1blacketer@gmail.com>
2026-02-22 01:35:38 +0000

    casting

---
+++ lesson2/classwork/casting.py
@@ -0,0 +1,15 @@
+num_str = "42"
+num_int = int(num_str) #converting a string to a intriger
+print(num_int + 1)
+
+num_str = 3.14 # cnverting a string to a float
+num_float = float(num_str)
+print(num_float)
+
+float_num = 9.99
+int_num = int(float_num) # converting a float to a intiger
+print(int_num)
+
+num = 100
+num_str2 = str(num) # converting a iniger to string
+print(num_str2)
